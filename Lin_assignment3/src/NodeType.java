ab165fc5e62afc9573cb950198841d0c54e7dbc9
Brandon Lin <168325202+BrandonKWLin@users.noreply.github.com>
2025-07-14 22:10:51 -0400

    Update NodeType.java

--- Lin_assignment3/src/NodeType.java
+++ Lin_assignment3/src/NodeType.java
@@ -1,7 +1,9 @@
 package cs2720;
 
 /**
-   public class NodeType.
+ * Generic tree node used in the Binary Search Tree.
+ *
+ * @param <T> the type of data stored in the node
  */
 public class NodeType<T extends Comparable<T>> {
 
@@ -9,4 +11,23 @@ public class NodeType<T extends Comparable<T>> {
     public NodeType<T> left;
     public NodeType<T> right;
 
-} // NodeType
+    /**
+     * Default constructor initializes an empty node.
+     */
+    public NodeType() {
+        this.info = null;
+        this.left = null;
+        this.right = null;
+    }
+
+    /**
+     * Constructs a node with a given value.
+     *
+     * @param info the data to store in the node
+     */
+    public NodeType(T info) {
+        this.info = info;
+        this.left = null;
+        this.right = null;
+    }
+}
